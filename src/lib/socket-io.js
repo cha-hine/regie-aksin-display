8929caf2a72bd00a3c6da2b207d17c98294aec49
cha-hine <chahinebadouraly@gmail.com>
2026-02-18 17:31:10 +0400

    -b

--- src/lib/socket-io.js
+++ src/lib/socket-io.js
@@ -1,7 +1,7 @@
 // src/services/socket.ts
 import { io } from 'socket.io-client'
 
-const socket = io(import.meta.env.VITE_STRAPI_API_URL, {
+const socket = io(import.meta.env.VITE_STRAPI_UPLOAD_URL, {
   transports: ['websocket'],
 })
 
